5cf2bcee45f49df67da71e29318e7ecdd60649ed
Mateus Leao <mateus@yalochat.com>
2021-12-02 19:30:23 +0100

    go_mysql_docker-v0.2-organize-init-db

--- go_mysql_docker/main.go
+++ go_mysql_docker/main.go
@@ -10,16 +10,26 @@ import (
 
 func getDb() (*sql.DB, error) {
 	fmt.Println("log: starting app, waiting 15 secs for database to start")
-	time.Sleep(30 * time.Second)
 	db, err := sql.Open("mysql", "root:example@tcp(mysqldb:3306)/testdb")
 	if err != nil {
 		return nil, err
 	}
 	//defer db.Close()  // making sure db will be closed
-	err = db.Ping()
-	if err != nil {
-		return nil, err
+	
+	for i:=0 ; i<=10; i++ {
+		err = db.Ping()
+		if err != nil {
+			fmt.Println("Connection failed, trying again in 5 secs, will retry 10 times")
+			time.Sleep(5 * time.Second)
+			continue
+		}
+		if i==10{
+			panic(err.Error()) 
+		}
+		fmt.Println("Connection successfull")
+		break // leave loop if successfully connected
 	}
+	
 	db.SetMaxOpenConns(20)
     db.SetMaxIdleConns(20)
     db.SetConnMaxLifetime(time.Minute * 5)
@@ -33,6 +43,13 @@ func main() {
         panic(err.Error())
         return
     }
+
+	// create table
+	createTable, err := db.Query("CREATE TABLE IF NOT EXISTS users (id int primary key auto_increment, name text)")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer createTable.Close()
 	insert, err := db.Query("INSERT INTO users (name) values ('Ted')")
 	if err != nil {
 		panic(err.Error())
