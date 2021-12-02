ca1dc5217b901b395b975a01d23c6ad5e947f3fc
Mateus Leao <mateus@yalochat.com>
2021-12-02 19:04:21 +0100

    go_mysql_docker-v0.2-reading-data

--- go_mysql_docker/main.go
+++ go_mysql_docker/main.go
@@ -1,27 +1,66 @@
 package main
 
 import (
+	"log"
 	"time"
 	"fmt"
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-
-func main() {
+func getDb() (*sql.DB, error) {
 	fmt.Println("log: starting app, waiting 15 secs for database to start")
-	time.Sleep(15 * time.Second)
+	time.Sleep(30 * time.Second)
 	db, err := sql.Open("mysql", "root:example@tcp(mysqldb:3306)/testdb")
 	if err != nil {
-		panic(err.Error())
+		return nil, err
 	}
+	//defer db.Close()  // making sure db will be closed
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	db.SetMaxOpenConns(20)
+    db.SetMaxIdleConns(20)
+    db.SetConnMaxLifetime(time.Minute * 5)
 	fmt.Println("Successfully connected to mysql")
-	defer db.Close()  // making sure db will be closed
+	return db, nil
+}
 
-	insert, err := db.Query("INSERT INTO people (name) values ('Ted')")
+func main() {
+	db, err := getDb()
+	if err != nil {
+        panic(err.Error())
+        return
+    }
+	insert, err := db.Query("INSERT INTO users (name) values ('Ted')")
 	if err != nil {
 		panic(err.Error())
 	}
+	fmt.Println("Successfully added 1 person into 'users' table")
 	defer insert.Close()
-	fmt.Println("Successfully added 1 person into people's table")
+
+	// Querying database and returning the data row by row
+	var (
+		id int
+		name string
+	)
+
+	rows, err := db.Query("select id, name from users")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(id, name)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	
 }
